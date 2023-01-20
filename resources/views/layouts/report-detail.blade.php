c87b92c97d38a48526087d89c3abc651e02db4b6
muhamadijlal <haidarijlal027@gmail.com>
2023-01-21 05:18:01 +0700

    report detail responsive table

--- resources/views/layouts/report-detail.blade.php
+++ resources/views/layouts/report-detail.blade.php
@@ -12,26 +12,28 @@
       <h5 class="mb-0">Detail Data Periode {{ $data->periode }}</h5>
     </div>
     <div class="card-body">
-      <table class="table table-bordered">
-        <thead>
-          <tr>
-            <th>No</th>
-            <th>Periode</th>  
-            @foreach ($all as $item)
-              <th>{{ $item->prodi }}</th>
-            @endforeach
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>{{ $data->periode }}</td>          
-            @foreach ($all as $item)            
-              <td>{{ $item->total_prodi }}</td>
-            @endforeach
-          </tr>
-        </tbody>
-      </table>
+      <div class="table-responsive">
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Periode</th>  
+              @foreach ($all as $item)
+                <th>{{ $item->prodi }}</th>
+              @endforeach
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>{{ $data->periode }}</td>          
+              @foreach ($all as $item)            
+                <td>{{ $item->total_prodi }}</td>
+              @endforeach
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 
@@ -40,26 +42,28 @@
       <h5 class="mb-0">Detail Data Periode mahasiswa reguler {{ $data->periode }}</h5>
     </div>
     <div class="card-body">
-      <table class="table table-bordered">
-        <thead>
-          <tr>
-            <th>No</th>
-            <th>Periode</th>
-            @foreach ($reguler as $item)
-              <th>{{ $item->prodi }}</th>
-            @endforeach
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>{{ $data->periode }}</td>          
-            @foreach ($reguler as $item)            
-              <td>{{ $item->total_prodi }}</td>
-            @endforeach        
-          </tr>
-        </tbody>
-      </table>
+      <div class="table-responsive">
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Periode</th>
+              @foreach ($reguler as $item)
+                <th>{{ $item->prodi }}</th>
+              @endforeach
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>{{ $data->periode }}</td>          
+              @foreach ($reguler as $item)            
+                <td>{{ $item->total_prodi }}</td>
+              @endforeach        
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 
@@ -68,26 +72,28 @@
       <h5 class="mb-0">Detail Data Periode mahasiswa transfer {{ $data->periode }}</h5>
     </div>
     <div class="card-body">
-      <table class="table table-bordered">
-        <thead>
-          <tr>
-            <th>No</th>
-            <th>Periode</th>
-            @foreach ($transfer as $item)
-              <th>{{ $item->prodi }}</th>
-            @endforeach
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>{{ $data->periode }}</td>          
-            @foreach ($transfer as $item)            
-              <td>{{ $item->total_prodi }}</td>
-            @endforeach        
-          </tr>
-        </tbody>
-      </table>
+      <div class="table-responsive">
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Periode</th>
+              @foreach ($transfer as $item)
+                <th>{{ $item->prodi }}</th>
+              @endforeach
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>{{ $data->periode }}</td>          
+              @foreach ($transfer as $item)            
+                <td>{{ $item->total_prodi }}</td>
+              @endforeach        
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
   
